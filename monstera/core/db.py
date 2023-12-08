38732275b794852949156818b45fdd0c6f72303d
jonathansickert <jonathan.sickert@gmail.com>
2023-12-08 17:41:41 +0100

    added db resource

---
+++ monstera/core/db.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import Any
+
+import pandas as pd
+from sqlalchemy import CursorResult, Engine, Executable, Select, Table, create_engine
+
+__all__: list[str] = ["DBResource", "db"]
+
+
+@dataclass
+class DBResource:
+    url: str
+
+    @property
+    def engine(self) -> Engine:
+        return create_engine(self.url)
+
+    def query_df(self, stmt: Select) -> pd.DataFrame:
+        with self.engine.begin() as conn:
+            return pd.read_sql(sql=stmt, con=conn)
+
+    def write_df(self, table: Table, df: pd.DataFrame) -> int:
+        records_written: int | None = 0
+        with self.engine.begin() as conn:
+            records_written = df.to_sql(
+                name=table.name,
+                con=conn,
+                if_exists="append",
+                index=False,
+            )
+        # self.execute(insert(table).values(df.to_dict(orient="records")))
+        return 0 if records_written is None else records_written
+
+    def purge(self, table: Table) -> None:
+        with self.engine.begin() as conn:
+            conn.execute(table.delete())
+
+    def execute(self, stmt: Executable) -> CursorResult[Any]:
+        with self.engine.begin() as conn:
+            return conn.execute(statement=stmt)
+
+
+db = DBResource(
+    url="sqlite:////Users/jonathansickert/git/monstera/monstera/core/database.db"
+)
