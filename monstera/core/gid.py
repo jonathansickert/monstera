0e1cd0689aa1ef123fdb372df13e9dee3fb8dba8
jonathansickert <jonathan.sickert@gmail.com>
2023-12-09 11:37:10 +0100

    added expense gid

--- monstera/core/gid.py
+++ monstera/core/gid.py
@@ -6,6 +6,7 @@ __all__: list[str] = ["GID"]
 
 class GID(Enum):
     CATEGRORIES = "91edbbf6-2ecb-4f49-a08d-94888ce3735d"
+    EXPENSES = "381eb60d-a7bc-46e9-b74f-f28f994760b8"
 
     def create(self, *args) -> str:
         name: str = "".join(str(a) for a in args)
